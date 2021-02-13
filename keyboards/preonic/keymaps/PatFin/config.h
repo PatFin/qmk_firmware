5adf7c709238306c4b32d94b314ba04f6cba93a4
Patrick Finnerty <finnertypatri@aol.fr>
2021-02-13 19:58:18 +0900

    Added documentation to Keymap.c and header file.

--- keyboards/preonic/keymaps/PatFin/config.h
+++ keyboards/preonic/keymaps/PatFin/config.h
@@ -19,7 +19,6 @@
 /* enable basic MIDI features:
    - MIDI notes can be sent when in Music mode is on
 */
-
 #define MIDI_BASIC
 
 /* enable advanced MIDI features:
@@ -34,8 +33,23 @@
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 
-/* Set the number of taps used to set a layer on with TT from 5 to 2 */
+/* Set the number of taps used to set a layer on with TT from the default 5 to 2 */
 #define TAPPING_TOGGLE 2
 
-/* Set the input mode for unicode */
-#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC
+/*
+ * Set the input mode for unicode characters to match the operating systems
+ * Choose the line that matches your need and keep the others commented out.
+ *
+ * On Linux systems, you shouldn't need any particular settings for unicode input to work with iBus.
+ * On MAC systems, some settings need to be activated for this feature to function.
+ * Likewise on Windows, you will need to install "WinCompese". You will find more information on
+ * this page:
+ * https://beta.docs.qmk.fm/using-qmk/software-features/feature_unicode#2-input-modes-id-input-modes
+ */
+//#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC, UC_MAC // Use this if you use Windows, MAC, and Linux
+#define UNICODE_SELECTED_MODES UC_LNX, UC_WINC           // Use this if you use Windows and Linux
+//#define UNICODE_SELECTED_MODES UC_WINC, UC_MAC         // Use this if you use Windows and MAC
+//#define UNICODE_SELECTED_MODES UC_LNX, UC_MAC          // Use this if you use MAC and Linux
+//#define UNICODE_SELECTED_MODES UC_WINC                 // Use this if you only use Windows
+//#define UNICODE_SELECTED_MODES UC_LNX                  // Use this if you only use Linux
+//#define UNICODE_SELECTED_MODES UC_MAC                  // USe this if you only use MAC
